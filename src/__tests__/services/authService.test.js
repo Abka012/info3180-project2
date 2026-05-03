5a18c741cb106d02274afdf90ee810fc80e7f215
Abka Ferguson <fergusonabka012@gmail.com>
2026-05-02 23:49:04 -0500

    fix: no more gitguardian security errors

--- src/__tests__/services/authService.test.js
+++ src/__tests__/services/authService.test.js
@@ -1,4 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
+import "dotenv/config";
 
 // Import actual functions to test
 import { validateEmail, passwordValidation } from "@/services/authService.js";
@@ -63,7 +64,9 @@ describe("authService - passwordValidation", () => {
     });
 
     it("returns false when missing special character", () => {
-      const result = passwordValidation.validate("mockPassword123");
+      const result = passwordValidation.validate(
+        process.env.MISSING_SPECIAL_CHARACTER_PASSWORD,
+      );
       expect(result.isValid).toBe(false);
       expect(result.errors).toContain(
         "Password must contain at least one special character",
@@ -71,7 +74,7 @@ describe("authService - passwordValidation", () => {
     });
 
     it("returns strength in result", () => {
-      const result = passwordValidation.validate("dummyPass_123!");
+      const result = passwordValidation.validate(process.env.STRONG_PASSOWRD);
       expect(result.strength).toBeDefined();
       expect(["weak", "medium", "strong"]).toContain(result.strength);
     });
